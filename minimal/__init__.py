4993f01ff7c1948aa1d89b295676b34ad2e6e316
LuisHRiosC18 <141874238+LuisHRiosC18@users.noreply.github.com>
2023-11-22 14:59:13 -0600

    FIrst commit

---
+++ minimal/__init__.py
@@ -0,0 +1,13 @@
+def my_sum(x, y):
+    """A function that sums. """
+    return x+y
+
+
+def my_mul(x, y):
+    """A function that multiply. """
+    return x*y
+
+def everybody():
+    for i in range(0,9):
+        print('EVERYBODYYYYYYY')
+    return 
